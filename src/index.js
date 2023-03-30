bbaf26ddfbfd73a018a88b051aecbcd264fb0dc9
Andrew <https://github.com/AndreyTantsiura>
2023-03-30 08:20:32 +0300

    feat: add project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import daniel from './img/daniel-leone.jpg';
+// import dirk from './img/dirk-von-loen.jpg';
+// import greg from './img/greg-jurgajtis.jpg';
+// import marc from './img/marc-pell.jpg';
+// import tom from './img/tom-winckels.jpg';
+
+
+const bottomPrev = document.querySelector(".bottom_prev");
+console.log(bottomPrev)
