2d973ac399f2f7fcb5bd56a4205835a79e955dd4
Andrew <https://github.com/AndreyTantsiura>
2023-04-05 23:19:49 +0300

    feat: added render section3

--- src/index.js
+++ src/index.js
@@ -51,8 +51,8 @@ const headerButtonNext = document.querySelector('.button_next');
 const counterCurrent = document.querySelector('.counter_current');
 const headerTitle = document.querySelector('.header_title > h1');
 const headerDescribe = document.querySelector('.header_title > p');
+//render header
 let currentIndex = 0;
-
 const renderHeader = () => {
   header.style.backgroundImage = `linear-gradient(to bottom, rgb(51, 51, 51, 0.3), rgb(51, 51, 51)), url(${arrImg[currentIndex].img})`;
   headerTitle.textContent = `${arrImg[currentIndex].title}`;
@@ -88,37 +88,88 @@ const section1Country3 = document.querySelector('.content_block3 > h3');
 const section1Info1 = document.querySelector('.content_block1 > p');
 const section1Info2 = document.querySelector('.content_block2 > p');
 const section1Info3 = document.querySelector('.content_block3 > p');
-const section1CounterCurrent = document.querySelector('.section1_bottom .counter_current');
-
-let currentIndex1 = 0;
-let currentIndex2 = 1;
-let currentIndex3 = 2;
+const section1CounterCurrent = document.querySelector(
+  '.section1_bottom .counter_current'
+);
+//render section1
+let currentIndex1 = 1;
+let currentIndex2 = 2;
 const renderSection1 = () => {
-  block1Img.setAttribute('src', `${arrImg[currentIndex1].img}`);
-  block2Img.setAttribute('src', `${arrImg[currentIndex2].img}`);
-  block3Img.setAttribute('src', `${arrImg[currentIndex3].img}`);
-  section1Title1.textContent = `${arrImg[currentIndex1].title}`;
-  section1Title2.textContent = `${arrImg[currentIndex2].title}`;
-  section1Title3.textContent = `${arrImg[currentIndex3].title}`;
-  section1Country1.textContent = `${arrImg[currentIndex1].country}`;
-  section1Country2.textContent = `${arrImg[currentIndex2].country}`;
-  section1Country3.textContent = `${arrImg[currentIndex3].country}`;
-  section1Info1.textContent = `${arrImg[currentIndex1].info}`;
+  block1Img.setAttribute('src', `${arrImg[currentIndex].img}`);
+  block2Img.setAttribute('src', `${arrImg[currentIndex1].img}`);
+  block3Img.setAttribute('src', `${arrImg[currentIndex2].img}`);
+  section1Title1.textContent = `${arrImg[currentIndex].title}`;
+  section1Title2.textContent = `${arrImg[currentIndex1].title}`;
+  section1Title3.textContent = `${arrImg[currentIndex2].title}`;
+  section1Country1.textContent = `${arrImg[currentIndex].country}`;
+  section1Country2.textContent = `${arrImg[currentIndex1].country}`;
+  section1Country3.textContent = `${arrImg[currentIndex2].country}`;
+  section1Info1.textContent = `${arrImg[currentIndex].info}`;
+  section1Info3.textContent = `${arrImg[currentIndex1].info}`;
   section1Info3.textContent = `${arrImg[currentIndex2].info}`;
-  section1Info3.textContent = `${arrImg[currentIndex3].info}`;
-  section1CounterCurrent.textContent = `${arrImg[currentIndex1].id}`;
+  section1CounterCurrent.textContent = `${arrImg[currentIndex].id}`;
 };
 //render section1 by click previous button
 section1ButtonPrev.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % arrImg.length;
   currentIndex1 = (currentIndex1 + 1) % arrImg.length;
   currentIndex2 = (currentIndex2 + 1) % arrImg.length;
-  currentIndex3 = (currentIndex3 + 1) % arrImg.length;
   renderSection1();
 });
 //render section1 by click next button
 section1ButtonNext.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + arrImg.length) % arrImg.length;
+  currentIndex1 = (currentIndex1 - 1 + arrImg.length) % arrImg.length;
+  currentIndex2 = (currentIndex2 - 1 + arrImg.length) % arrImg.length;
+  renderSection1();
+});
+
+const image1 = document.querySelector('.image_1 img');
+const image2 = document.querySelector('.image_2 img');
+const image3 = document.querySelector('.image_3 img');
+const image4 = document.querySelector('.image_4 img');
+const image5 = document.querySelector('.image_5 img');
+const section3ButtonPrev = document.querySelector(
+  '.section3_bottom .button_prev'
+);
+const section3ButtonNext = document.querySelector(
+  '.section3_bottom .button_next'
+);
+const section3Title = document.querySelector('.title_img > h2');
+const section3Country = document.querySelector('.title_img > h3');
+const section3Info = document.querySelector('.info_img > p');
+const section3CounterCurrent = document.querySelector(
+  '.section3_bottom .counter_current'
+);
+
+let currentIndex3 = 3;
+let currentIndex4 = 4;
+const renderSection3 = () => {
+  image1.setAttribute('src', `${arrImg[currentIndex].img}`);
+  image2.setAttribute('src', `${arrImg[currentIndex1].img}`);
+  image3.setAttribute('src', `${arrImg[currentIndex2].img}`);
+  image4.setAttribute('src', `${arrImg[currentIndex3].img}`);
+  image5.setAttribute('src', `${arrImg[currentIndex4].img}`);
+  section3Title.textContent = `${arrImg[currentIndex].title}`;
+  section3Country.textContent = `${arrImg[currentIndex].country}`;
+  section3Info.textContent = `${arrImg[currentIndex].info}`;
+  section3CounterCurrent.textContent = `${arrImg[currentIndex].id}`;
+};
+//render section3 by click previous button
+section3ButtonPrev.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % arrImg.length;
+  currentIndex1 = (currentIndex1 + 1) % arrImg.length;
+  currentIndex2 = (currentIndex2 + 1) % arrImg.length;
+  currentIndex3 = (currentIndex3 + 1) % arrImg.length;
+  currentIndex4 = (currentIndex4 + 1) % arrImg.length;
+  renderSection3();
+});
+//render section3 by click next button
+section3ButtonNext.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + arrImg.length) % arrImg.length;
   currentIndex1 = (currentIndex1 - 1 + arrImg.length) % arrImg.length;
   currentIndex2 = (currentIndex2 - 1 + arrImg.length) % arrImg.length;
   currentIndex3 = (currentIndex3 - 1 + arrImg.length) % arrImg.length;
-  renderSection1();
+  currentIndex4 = (currentIndex4 - 1 + arrImg.length) % arrImg.length;
+  renderSection3();
 });
